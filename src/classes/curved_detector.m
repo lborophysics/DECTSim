397d77785c6c2ae7564b1d6eedae552eb7c15fc2
Joshua Gray <61458311+jgray-19@users.noreply.github.com>
2023-11-22 22:02:25 +0000

    Update detectors to include working curved detector Add tests for detectors Move more functions to base class

--- src/classes/curved_detector.m
+++ src/classes/curved_detector.m
@@ -1,46 +1,59 @@
-classdef curved_detector
+classdef curved_detector < detector
     properties
-        pixel_angle
-        detector_angle
-        vec_to_detector
+        init_to_source_vec (3, 1) double % The vector from the left edge of the detector to the source
+        source_position    (3, 1) double % The position of the source
+        pixel_angle        (1, 1) double % The angle covered by each pixel
     end
-    
-    properties(Access = private)
-        rot_matrix
-    end
-
     methods
-        function obj = curved_detector(vec_to_detector, dist_to_detector, detector_angle, detector_size)
+        function self = curved_detector(dist_to_detector, detector_angle, pixel_angle, rotation_angle)
             arguments
-                vec_to_detector (3, 1) double
-                dist_to_detector       double
-                detector_angle         double
-                detector_size          double
+                dist_to_detector double
+                detector_angle   double
+                % detector_width   double
+                pixel_angle      double
+                rotation_angle   double = pi/180
             end
-            obj.pixel_angle     = detector_size / dist_to_detector;
-            obj.detector_angle  = detector_angle;
-            obj.rot_matrix      = rotz(-detector_angle / 2);
-            obj.vec_to_detector = obj.rot_matrix * vec_to_detector; % rotate to edge of detector
-        end
-        
-        function pixel = get_hit_pixel(obj, ray_unit_vector)
-            arguments
-                obj
-                ray_unit_vector (3, 1) double
-            end
-            % get the angle between the ray and the detector
-            angle = acos(dot(ray_unit_vector, obj.vec_to_detector));
+            % detector_angle = 2 * asin(detector_width / (2 * dist_to_detector));
 
-            % get pixel number
-            pixel = floor(angle / obj.pixel_angle) + 1;
+            self@detector(dist_to_detector, rotation_angle, detector_angle/pixel_angle, 2*pi);
+            self.pixel_angle    = pixel_angle;
+            
+            % Only true for initial configuration
+            self.init_to_source_vec = rotz(-detector_angle/2) * self.to_source_vec; 
+            self.source_position    = self.to_source_vec * dist_to_detector/2;
         end
 
-        function obj = move_detector(obj, new_direction) % How can I make this fast?
+        function self = rotate(self)
+            % rotate the detector by the rotation angle
+            self.init_to_source_vec = self.rot_mat * self.init_to_source_vec;
+            self.source_position    = self.rot_mat * self.source_position;
+        end
+
+        function ray_generator = get_ray_generator(self, ray_per_pixel)
+            % Create a function which returns the rays which should be fired to hit each pixel.
+            % Only 1 ray per pixel is supported at the moment, as anti-aliasing techniques are not yet implemented.
             arguments
-                obj           curved_detector
-                new_direction (3, 1) double
+                self           curved_detector
+                ray_per_pixel  int32             = 1
             end
-            obj.vec_to_detector = obj.rot_matrix * new_direction;
-        end 
+            assert(nargin==1, "Only 1 ray per pixel is supported at the moment, as anti-aliasing techniques are not yet implemented.")
+
+            % Cache some variables to make the function faster
+            pixel_angle      = self.pixel_angle;
+            dist_to_detector = self.dist_to_detector;
+            num_pixels       = self.num_pixels;
+            to_source_vec    = rotz(pixel_angle / 2) * self.init_to_source_vec;
+            source_pos       = self.source_position;
+
+            % Create the function which returns the rays
+            ray_generator = @generator;
+            function xray = generator(pixel)            
+                assert(pixel <= num_pixels && pixel > 0, "Pixel number must be between 1 and the number of pixels on the detector.")
+                pixel_vec = rotz(pixel_angle * (pixel - 1)) * to_source_vec;
+                xray = ray(source_pos, -pixel_vec, dist_to_detector);
+            end
+        end
+                
+
     end
 end
--- src/classes/detector.m
+++ src/classes/detector.m
@@ -1,31 +1,39 @@
 classdef detector
-    properties
-        pixel_width (1, 1) double
-        width       (1, 1) double
-        centre      (3, 1) double
-        num_pixels         double
-        num_rotations      double
+    properties (Access=protected) % Do all these need to be stored? Could be calculated on the fly?
+        dist_to_detector (1, 1) double % Distance from source to detector
+        num_pixels       (1, 1) double % Number of pixels in the detector
+        num_rotations    (1, 1) double % Number of rotations around the object
+        rot_mat          (3, 3) double % Matrix for each individual rotation of the detector around the object
+        total_rotation   (1, 1) double % The maximum rotation of the detector around the object
+        to_source_vec    (3, 1) double = [0;1;0] % Vector from source to centre of detector
     end
 
     methods
 
-        function ray_generator = get_ray_generator(self, ray_per_pixel)
-            % get_ray_generator  Get a ray generator for this detector template
-            error('Not implemented for base class, must be implemented in subclass');
-        end
+        function self = detector(dist_to_detector, rotation_angle, num_pixels, total_rotation)
+            % detector  Construct a detector object
+            arguments
+                dist_to_detector (1, 1) double
+                rotation_angle   (1, 1) double
+                num_pixels       (1, 1) double
+                total_rotation   (1, 1) double
+            end
+            self.dist_to_detector = dist_to_detector;
+            self.total_rotation   = total_rotation;
+            self.num_pixels       = num_pixels;
+            if mod(num_pixels, 1) ~= 0
+                assert(abs(mod(num_pixels, 1)) < 1e-12, 'Number of pixels must be an integer, got %f', num_pixels);
+                self.num_pixels = round(num_pixels);
+            end
 
+            % Define how the detector should be rotated
+            self.rot_mat       = rotz(rotation_angle);
+            self.num_rotations = ceil(total_rotation / rotation_angle); % default to 180 degrees
+        end
+        
         function rotate(self)
             % rotate  Rotate the detector by one rotation
             error('Not implemented for base class, must be implemented in subclass');
-        end
-
-        function pixel = detector_response(~, attenuation)
-            % detector_response  Calculate the detector response for a given attenuation coefficient
-            arguments
-                ~
-                attenuation (1, 1) double
-            end
-            pixel = exp(-attenuation);
         end
 
         function image = generate_image(self, voxels)
@@ -43,6 +51,28 @@ classdef detector
                 self = self.rotate();
             end
             image = mat2gray(-log(image'));
-        end            
+        end
+        
+        function scan_angles = get_scan_angles(self)
+            % Get the angles at which the detector should be rotated to scan the object (in degrees)
+            scan_angles = rad2deg(linspace(0, self.total_rotation, self.num_rotations+1));
+            scan_angles = scan_angles(1:end-1);
+        end
+
+    % Protect the following methods?
+    
+        function ray_generator = get_ray_generator(self, ray_per_pixel)
+            % get_ray_generator  Get a ray generator for this detector template
+            error('Not implemented for base class, must be implemented in subclass');
+        end
+
+        function pixel = detector_response(~, attenuation)
+            % detector_response  Calculate the detector response for a given attenuation coefficient
+            arguments
+                ~
+                attenuation (1, 1) double
+            end
+            pixel = exp(-attenuation);
+        end
     end
 end
