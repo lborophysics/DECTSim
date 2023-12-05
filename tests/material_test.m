2b863e8d454840c2f76d328955cbe0c3ab67b276
Joshua Gray <61458311+jgray-19@users.noreply.github.com>
2023-12-05 16:52:59 +0000

    Add water to materials and test it

--- tests/material_test.m
+++ tests/material_test.m
@@ -1,12 +1,15 @@
 classdef material_test < matlab.unittest.TestCase
 
     methods (Test)
-        function test_init(tc)
-            m = material(10);
-            tc.verifyEqual(m.mu, 10);
-
-            m = material(20);
-            tc.verifyEqual(m.mu, 20);
+        function test_water(tc)
+            tc.verifyEqual(water(1e-3), 4.078E+03)
+            tc.verifyEqual(water(1.5e-2), 1.673E+00)
+            tc.verifyEqual(water(8e-2), 1.837E-01)
+            tc.verifyEqual(water(1.25), 6.323E-02)
+            tc.verifyEqual(water(2e1),1.813E-02)
+            
+            tc.verifyWarning(@ () water(9e-4), '')
+            tc.verifyWarning(@ () water(2.1e1), '')
         end
     end
 
