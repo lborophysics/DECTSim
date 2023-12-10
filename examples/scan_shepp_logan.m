67eba96549401848e2a38084a540123335507cd0
Joshua Gray <61458311+jgray-19@users.noreply.github.com>
2023-12-10 15:03:48 +0000

    Update Example and add DukeSim Parser

--- examples/scan_shepp_logan.m
+++ examples/scan_shepp_logan.m
@@ -2,21 +2,25 @@
 vox_arr_center = zeros(3, 1); 
 phantom_size = 0.4; 
 voxel_size = 1e-4;
+material = @water;
 
 % Create voxel array
-voxel_generator = voxel_shepp_logan(vox_arr_center, phantom_size, voxel_size);
-% voxel_generator = voxel_box(vox_arr_center, phantom_size);
+% voxel_generator = voxel_shepp_logan(vox_arr_center, phantom_size, voxel_size);
+
+% voxel_generator = voxel_box(vox_arr_center, phantom_size, material);
+voxel_generator = voxel_cylinder(vox_arr_center, phantom_size/2, 2, material);
+
 voxels = voxel_array(vox_arr_center, zeros(3, 1)+phantom_size, voxel_size, voxel_generator);
 
 do_parallel = true;
 do_curved = false;
 
 if do_parallel 
-    profile on
+    % profile on
     % Parallel Detector constants
     dist_to_detector = 10;
     detector_width = 0.8;
-    pixel_width = 2e-3;
+    pixel_width = 1e-3;
     rotation_angle = pi/180;
 
     my_detector = parallel_detector(dist_to_detector, detector_width, pixel_width, rotation_angle);
@@ -28,7 +32,7 @@ if do_parallel
     [R, H] = iradon(image, scan_angles);%, "linear", "None");
 
     imwrite(mat2gray(R), "shepp_logan_reproduced_p.png")
-    profile viewer
+    % profile viewer
 end
 
 if do_curved
@@ -37,14 +41,16 @@ if do_curved
     dist_to_detector = 10;
     detector_angle = pi/20;
     pixel_angle = detector_angle/500;
-    rotation_angle = pi/720;
+    rotation_angle = pi/360;
 
     my_detector = curved_detector(dist_to_detector, detector_angle, pixel_angle, rotation_angle);
     image = my_detector.generate_image(voxels);
+    % image = my_detector.generate_image_p(voxels);
+
 
     imwrite(image, "sinograph_c.png")
 
-    [P,paraSensorPos,paraRotAngles] = fan2para(image, dist_to_detector/2, 'FanSensorSpacing', rad2deg(pixel_angle), 'FanRotationIncrement', rad2deg(rotation_angle), "ParallelCoverage", "cycle");
+    [P,paraSensorPos,paraRotAngles] = fan2para(image, dist_to_detector/2, 'FanSensorSpacing', rad2deg(pixel_angle), 'FanRotationIncrement', rad2deg(rotation_angle));%, "ParallelCoverage", "cycle");
 
     scan_angles = my_detector.get_scan_angles();
     [R, H] = iradon(P, paraRotAngles);%, "linear", "None");
