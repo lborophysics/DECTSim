f8e747efaa8508b61d59bac266f3f152a785b5f4
Joshua Gray <61458311+jgray-19@users.noreply.github.com>
2024-02-04 17:17:39 +0100

    Add cross section and mean free path calculation

--- src/get_material.m
+++ src/get_material.m
@@ -3,13 +3,14 @@ classdef get_material
     % for ray tracing and scattering
     
     properties
-        get_mu
+        get_mu        % Function handle to get the linear attenuation coefficient of the material for a given energy
+        material_id   % Unique identifier of the material, generated from the material name
     end
-    
+
     properties (Access=protected)
-        atomic_numbers
-        mass_fractions
-        density
+        atomic_numbers % Atomic numbers of the elements in the material
+        mass_fractions % Mass fractions of the elements in the material
+        density        % Density of the material in g/cm^3
     end
 
     properties (Access=private, Constant)
@@ -47,19 +48,16 @@ classdef get_material
             % Pure and Applied Chemistry, vol. 94, no. 5, 2022, pp. 573-600. 
             % https://doi.org/10.1515/pac-2019-0603
         avogadro_number = 6.02214076 * 10^23; % atoms/mol, Source: https://pml.nist.gov/cgi-bin/cuu/Value?na
+        em_ee = 510.99895; % KeV, Source: https://pml.nist.gov/cgi-bin/cuu/Value?mec2mev
     end
 
     methods
-        function self = get_material(varargin)
+        function self = get_material(material_name, varargin)
             %MATERIAL Construct a material object
             %   material("material_name") creates a material object with the properties of the given material, if it is available
-            %   material(atomic_numbers, mass_fractions, density) creates a material object with the given properties, using the PhotonAttenuation package
-            if nargin < 1
-                aac = matlab.lang.correction.AppendArgumentsCorrection('"blood"');
-                error(aac, 'MATLAB:notEnoughInputs', 'Not enough input arguments.') 
-            elseif nargin == 1
-                material_name = varargin{1};
-                assert(isstring(material_name), 'assert:failure', 'When only one input is given, it should be a string with the name of the material.');
+            %   material("material_name", atomic_numbers, mass_fractions, density) creates a material object with the given properties, using the PhotonAttenuation package
+            assert(isstring(material_name), 'assert:failure', 'The material name must be a string.');
+            if nargin == 1
                 material_index = find(self.known_materials == lower(material_name));
                 if any(material_index)
                     self.atomic_numbers = self.known_atomic_numbers{material_index};
@@ -70,7 +68,7 @@ classdef get_material
                         'The material %s is not available. Available materials are: %s', ...
                         material_name, strjoin(self.known_materials, ', '));
                 end
-            elseif nargin == 3
+            elseif nargin == 4
                 self.atomic_numbers = varargin{1};
                 self.mass_fractions = varargin{2};
                 self.density = varargin{3};
@@ -80,10 +78,62 @@ classdef get_material
                 assert(isnumeric(self.density) && isscalar(self.density), 'assert:failure', 'The density should be a scalar number.');
                 self.mass_fractions = self.mass_fractions / sum(self.mass_fractions); 
             else
-                error('MATLAB:invalidInput', 'Invalid number of input arguments.');
+                error('MATLAB:invalidInput', 'Invalid number of input arguments. Use either material("material_name") or material("material_name", atomic_numbers, mass_fractions, density).');
             end
-            self.get_mu = @(E) sum(PhotonAttenuationQ(self.atomic_numbers, E, 1) .* self.mass_fractions) * self.density;
+            % Create a unique identifier for the material (spend time in creating this, but save time in the future by not having to compare strings)
+            alphabet = 'abcdefghijklmnopqrstuvwxyz'; Map(alphabet) = 1:length(alphabet);
+            self.material_id = bin2dec(convertCharsToStrings(dec2bin(Map(convertStringsToChars(lower(material_name))))));
+            
+            % Set the function handle to get the linear attenuation coefficient
+            self.get_mu = @(E) sum(PhotonAttenuationQ(self.atomic_numbers, E/1000, 1) .* self.mass_fractions) * self.density; %Convert energy from MeV to KeV
+        end
+
+        function cs = get_cross_section(self, Z, E)
+            %GET_CROSS_SECTION Get the cross section of the material for a given energy (CREDIT: Geant4)
+            % The values, formulae and code is taken directly from 
+            % https://github.com/Geant4/geant4/blob/master/source/processes/electromagnetic/standard/src/G4KleinNishinaCompton.cc
+            if E < 0.1; cs = 0; return; end % Below 100 eV, we are beyond the limit of the cross section table -> 0
+            % See https://geant4-userdoc.web.cern.ch/UsersGuides/PhysicsReferenceManual/html/electromagnetic/gamma_incident/compton/compton.html
+
+            a = 20.0; b = 230.0; c = 440.0; % Unitless
+            d1= 2.7965e-25; d2=-1.8300e-25; % cm^2 (e-24 for the barn)
+            d3= 6.7527e-24; d4=-1.9798e-23; % cm^2 (e-24 for the barn)
+            e1= 1.9756e-29; e2=-1.0205e-26; % cm^2 (e-24 for the barn)
+            e3=-7.3913e-26; e4= 2.7079e-26; % cm^2 (e-24 for the barn)
+            f1=-3.9178e-31; f2= 6.8241e-29; % cm^2 (e-24 for the barn)
+            f3= 6.0480e-29; f4= 3.0274e-28; % cm^2 (e-24 for the barn)
+            if Z < 1.5; T0 = 40; % Special case for hydrogen (KeV)
+            else;       T0 = 15; % KeV
+            end 
+            
+            X = max(E, T0) / self.em_ee; % Unitless
+            p1Z = Z*(d1 + e1*Z + f1*Z*Z); p2Z = Z*(d2 + e2*Z + f2*Z*Z); % cm^2
+            p3Z = Z*(d3 + e3*Z + f3*Z*Z); p4Z = Z*(d4 + e4*Z + f4*Z*Z); % cm^2
+            
+            cs = p1Z*log(1.+2.*X)/X + (p2Z + p3Z*X + p4Z*X*X)/(1. + a*X + b*X*X + c*X*X*X); % cm^2
+
+            if E < T0
+                X = (T0+1) / self.em_ee; % Unitless
+                sigma = p1Z*log(1.+2.*X)/X + (p2Z + p3Z*X + p4Z*X*X)/(1. + a*X + b*X*X + c*X*X*X); % cm^2
+                c1 = -T0*(sigma-cs)/cs; % Unitless
+                if Z > 1.5; c2 = 0.375-0.0556*log(Z); %Unitless 
+                else;       c2 = 0.150;               %Unitless
+                end 
+                y = log(E/T0); % Unitless
+                cs = cs * exp(-y*(c1+c2*y)); % cm^2
+            end
+        end
+
+        function mfp = get_mean_free_path(self, E)
+            % GET_MEAN_FREE_PATH Get the mean free path of the material for a given energy
+            imfp = 0;
+            for i = 1:length(self.atomic_numbers)
+                imfp = imfp + 1 / ...
+                    (self.avogadro_number * self.density * self.mass_fractions(i) ...
+                     * self.get_cross_section(self.atomic_numbers(i), E) ...
+                     / self.atomic_masses(self.atomic_numbers(i)));
+            end
+            mfp = 1 / imfp; % cm
         end
     end
 end
-
