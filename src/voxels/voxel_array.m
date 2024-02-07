7426cff14de1607aed43ab65ea0c8e2c2a8f16b6
Joshua Gray <61458311+jgray-19@users.noreply.github.com>
2024-02-07 20:03:02 +0100

    Improve how voxel arrays work (with removal of collections)

--- src/voxels/voxel_array.m
+++ src/voxels/voxel_array.m
@@ -1,32 +1,35 @@
-classdef voxel_array
+classdef voxel_array % The functions here need to be reviewed - are they all needed?
     properties
         array_position (3, 1) double % position of the top left corner of the array
         num_planes     (3, 1) double % number of planes in each dimension
         dimensions     (3, 1) double % dimensions of each voxel
-        voxel_obj
+        voxel_objs
+        nobj           (1, 1) double
         is_collection  (1, 1) logical
     end
-    
+
+    properties (Constant, NonCopyable)
+        air = material_attenuation("air");
+    end
+
     methods
-        function self = voxel_array(centre, object_dims, voxel_size, voxel_obj)
-            arguments
-                centre       (3, 1) double
-                object_dims  (3, 1) double 
-                voxel_size   (1, 1) double
-                voxel_obj    
-            end
+        function self = voxel_array(centre, object_dims, voxel_size, varargin)
             % Constructor method
             self.array_position = centre - object_dims ./ 2;
-            
+
             assert(voxel_size > 0, 'Voxel dimensions must be greater than zero')
             self.dimensions = ones(3, 1) * voxel_size;
 
-            assert(sum(object_dims ~= 0) == 3, 'There must be at least one length in each dimension')            
+            assert(sum(object_dims ~= 0) == 3, 'There must be at least one length in each dimension')
             self.num_planes = object_dims ./ voxel_size + 1; % +1 to include the last plane (fence post problem)
-            
+
             % In the future, I need to create this array based on the get_voxel_mu
-            self.voxel_obj = voxel_obj;
-            self.is_collection = isa(voxel_obj, 'voxel_collection');
+            % nobj = nargin - 3;
+            self.nobj = nargin - 3;
+            for obj = varargin
+                assert(isa(obj{1}, 'voxel_object'), 'All objects must be of type voxel_object')
+            end
+            self.voxel_objs = varargin;
         end
 
         function position = get_point_position(self, indices)
@@ -43,28 +46,57 @@ classdef voxel_array
             plane = self.array_position(coord) + (index - 1) * self.dimensions(coord);
         end
 
-        function mu = get_mu(self, i, j, k, energy)
+         function mu = get_mu(self, i, j, k, energy)
             % Convert indices to position at centre of voxel
             position = self.get_point_position([i; j; k]) + self.dimensions ./ 2;
 
             % Get mu at position
-            mu = self.voxel_obj.get_mu(position(1, :), position(2, :), position(3, :), energy);
-        end
-        
-        function [mat, id] = get_material(self, i, j, k)
-            % Get material at position
-            if self.is_collection
-                [mat, id] = self.voxel_obj.get_material(i, j, k);
-            else
-                mat = self.voxel_obj.material;
-                id = self.voxel_obj.id;
+            mu = zeros(1, length(i));
+            for obj = self.voxel_objs
+                mu(obj{1}.is_in_object(position(1, :), position(2, :), position(3, :))) =  ...
+                    obj{1}.get_mu(energy);
             end
         end
 
-        function mfp = get_mean_free_path(self, i, j, k, energy)
-            % Get mean free path of material at position
-            if self.is_collection; mfp = self.voxel_obj.get_mean_free_path(i, j, k, energy);
-            else;                  mfp = self.voxel_obj.get_mean_free_path(energy);
+        function mu_arr = get_mu_dict(self, energy)
+            % Create a dictionary of mu values for each material
+            mu_arr = zeros(1, self.nobj + 1);
+            for n = 1:self.nobj
+                mu_arr(n) = self.voxel_objs{n}.get_mu(energy);
+            end
+            mu_arr(self.nobj + 1) = self.air.get_mu(energy);
+        end
+
+        function mu = get_saved_mu(self, i, j, k, dict)
+            % Convert indices to position at centre of voxel
+            position = self.get_point_position([i; j; k]) + self.dimensions ./ 2;
+
+            % Get mu at position
+            mu = zeros(1, length(i)); % + dict(self.nobj + 1); % Default to air
+            for n = 1:self.nobj
+                mu(self.voxel_objs{n}.is_in_object(position(1, :), position(2, :), position(3, :))) =  ...
+                    dict(n);
+            end
+        end
+
+        function mfp_arr = get_mfp_dict(self, energy)
+            % Create a dictionary of mfp values for each material
+            mfp_arr = zeros(1, self.nobj + 1);
+            for n = 1:self.nobj
+                mfp_arr(n) = self.voxel_objs{n}.material.get_mean_free_path(energy);
+            end
+            mfp_arr(self.nobj + 1) = self.air.get_mean_free_path(energy);
+        end
+
+        function mfp = get_saved_mfp(self, i, j, k, dict)
+            % Convert indices to position at centre of voxel
+            position = self.get_point_position([i; j; k]) + self.dimensions ./ 2;
+
+            % Get material at position
+            mfp = zeros(1, length(i)) + dict(self.nobj + 1); % Default to air
+            for n = 1:self.nobj
+                mfp(self.voxel_objs{n}.is_in_object(position(1, :), position(2, :), position(3, :))) = ...
+                    dict(n);
             end
         end
     end
--- src/voxels/voxel_object.m
+++ src/voxels/voxel_object.m
@@ -2,17 +2,13 @@ classdef voxel_object
     properties
         is_in_object    % A function handle that determines whether a point is in the object
         material        % The material of the object
+        get_mu          % A function handle that returns the attenuation coefficient of the material
     end
     methods
         function self = voxel_object(is_in_object, material)
             self.is_in_object = is_in_object;
             self.material = material;
-        end
-
-        function mu = get_mu(self, i, j, k, energy)
-            mu = zeros(1, length(i));
-            is_in = self.is_in_object(i, j, k);
-            mu(is_in) = self.material.get_mu(energy);
+            self.get_mu = @(energy) material.get_mu(energy);
         end
     end
 end
