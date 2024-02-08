f3504071901a91a8b708f0c6c13753f214d929bd
Joshua Gray <61458311+jgray-19@users.noreply.github.com>
2024-02-08 13:15:05 +0100

    Improve coverage run to only include src

--- shortcuts/run_coverage.m
+++ shortcuts/run_coverage.m
@@ -3,8 +3,14 @@ import matlab.unittest.plugins.codecoverage.CoverageResult
 suite = testsuite();
 runner = testrunner('minimal');
 
+% get all m-code files under the folder
+out_dir = dir(fullfile("src", '**', '*.m'));
+mfile_paths = string({out_dir.folder}) + filesep + string({out_dir.name});
+% remove files to exclude
+mfile_paths(contains(mfile_paths, filesep+"misc"+filesep)) = [];
+
 format = CoverageResult;
-p = CodeCoveragePlugin.forFolder("src","IncludingSubfolders",true,"Producing", format);
+p = CodeCoveragePlugin.forFile(mfile_paths,"Producing", format);
 
 runner.addPlugin(p)
 
