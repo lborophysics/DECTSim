e8e71bb46e49b8c591ad1a759974f6a29d289c8b
jgray-19 <j.gray-19@student.lboro.ac.uk>
2024-02-14 17:02:22 +0000

    Simplify readability using units

--- src/materials/cross_section.m
+++ src/materials/cross_section.m
@@ -2,18 +2,18 @@ function cs = cross_section(Z, E) % Once tested change this function to evaluate
     % CROSS_SECTION Get the cross section of the material for a given energy (CREDIT: Geant4)
     % The values, formulae and code is taken directly from
     % https://github.com/Geant4/geant4/blob/master/source/processes/electromagnetic/standard/src/G4KleinNishinaCompton.cc
-    if E < 0.1; cs = 0; return; end % Below 100 eV, we are beyond the limit of the cross section table -> 0
+    if E < 100*units.eV; cs = 0; return; end % Below 100 eV, we are beyond the limit of the cross section table -> 0
     % See https://geant4-userdoc.web.cern.ch/UsersGuides/PhysicsReferenceManual/html/electromagnetic/gamma_incident/compton/compton.html
     
     persistent a b c d1 d2 d3 d4 e1 e2 e3 e4 f1 f2 f3 f4
     if isempty(a) % Initialize the constants if they are not already
         a = 20.0; b = 230.0; c = 440.0; % Unitless
-        d1= 2.7965e-25; d2=-1.8300e-25; % cm^2 (e-24 for the barn)
-        d3= 6.7527e-24; d4=-1.9798e-23; % cm^2 (e-24 for the barn)
-        e1= 1.9756e-29; e2=-1.0205e-26; % cm^2 (e-24 for the barn)
-        e3=-7.3913e-26; e4= 2.7079e-26; % cm^2 (e-24 for the barn)
-        f1=-3.9178e-31; f2= 6.8241e-29; % cm^2 (e-24 for the barn)
-        f3= 6.0480e-29; f4= 3.0274e-28; % cm^2 (e-24 for the barn)
+        d1= 2.7965e-1*units.barn; d2=-1.8300e-1*units.barn; % cm^2 (e-24 for the barn)
+        d3= 6.7527   *units.barn; d4=-1.9798e+1*units.barn; % cm^2 (e-24 for the barn)
+        e1= 1.9756e-5*units.barn; e2=-1.0205e-2*units.barn; % cm^2 (e-24 for the barn)
+        e3=-7.3913e-2*units.barn; e4= 2.7079e-2*units.barn; % cm^2 (e-24 for the barn)
+        f1=-3.9178e-7*units.barn; f2= 6.8241e-5*units.barn; % cm^2 (e-24 for the barn)
+        f3= 6.0480e-5*units.barn; f4= 3.0274e-4*units.barn; % cm^2 (e-24 for the barn)
     end
     T0 = zeros(size(Z)) + 40; % Special case for hydrogen (KeV)
     T0(Z > 1.5) = 15; % KeV
