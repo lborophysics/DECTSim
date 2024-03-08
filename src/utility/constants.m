f94d5007c045859d8a23b698163764b3185b3900
jgray-19 <j.gray-19@student.lboro.ac.uk>
2024-03-08 15:03:20 +0000

    Update units

--- src/utility/constants.m
+++ src/utility/constants.m
@@ -1,8 +1,8 @@
 classdef constants
     properties (Constant)
         % Physical constants
-        me_c2 = 510.99895; % KeV, Source: https://pml.nist.gov/cgi-bin/cuu/Value?mec2mev
+        me_c2 = 510.99895 .* units.keV; % KeV, Source: https://pml.nist.gov/cgi-bin/cuu/Value?mec2mev
         N_A = 6.02214076 * 10^23; % atoms/mol, Source: https://pml.nist.gov/cgi-bin/cuu/Value?na
-        c = 299792458; % m/s, Source: https://pml.nist.gov/cgi-bin/cuu/Value?c
+        c = 299792458 .* units.m / units.s; % m/s, Source: https://pml.nist.gov/cgi-bin/cuu/Value?c
     end
 end
