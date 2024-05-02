16258ad4854e59663f5fe36894f58d661f02c1bd
jgray-19 <j.gray-19@student.lboro.ac.uk>
2024-05-02 17:17:07 +0100

    Simplify and speed up ray tracing

--- src/ray_tracing/ray_trace_many.m
+++ src/ray_tracing/ray_trace_many.m
@@ -34,14 +34,16 @@ function [lengths, indices] = ray_trace_many(ray_start, v1_to_v2, init_plane, v_
     end
 
     % Ensure that the index is not less than 1 (this can happen due to floating point errors)
-    index_min = max(                                                         ...
-        floor(num_planes -                                                   ... 
-            (last_plane - ray_start - a_min_coord .* v1_to_v2) ./ v_dims), 1);
+    index_min = max(                                                                         ...
+        floor(num_planes -                                                            ... 
+            (last_plane - ray_start - a_min_coord .* v1_to_v2) ./ v_dims), ...
+        [1;1;1]                                                                              ...
+        );
 
     index_max = min(...
         ceil(1 + (ray_start - init_plane + a_max_coord .* v1_to_v2) ./ v_dims), ...
         num_planes);
-
+    
     % Refill the arrays with NaNs
     i_min = zeros(3, num_rays);
     i_min(:, not_nan_list) = index_min;
@@ -75,43 +77,32 @@ function [lengths, indices] = ray_trace_many(ray_start, v1_to_v2, init_plane, v_
     end
 end
 
-function [ls, idxs] = ray_trace_single(ray_start, v1_to_v2, init_plane, v_dims, index_min, index_max, a_min, a_max)
-    v_min = init_plane + v_dims .* (index_min - 1);
+function [ls, idxs] = ray_trace_single(ray_start, v1_to_v2, init_plane, v_dims, index_min, index_max, a_min, a_max)   
+        v1_to_v2 = round(v1_to_v2*1e10)/1e10;
+        to_plane = init_plane - ray_start;
 
-    a_set_x = get_set_a(ray_start(1), v_min(1), v_dims(1), index_min(1), index_max(1), v1_to_v2(1));
-    a_set_y = get_set_a(ray_start(2), v_min(2), v_dims(2), index_min(2), index_max(2), v1_to_v2(2));
-    a_set_z = get_set_a(ray_start(3), v_min(3), v_dims(3), index_min(3), index_max(3), v1_to_v2(3));
-    a = rmmissing(unique([a_set_x, a_set_y, a_set_z, a_min, a_max]));
-    a = a(a >= a_min & a <= a_max); % Remove any values outside the range
-
-    len_a = length(a);
-    d_12 = norm(v1_to_v2);
+        if     v1_to_v2(1) == 0; xrange = [];
+        elseif v1_to_v2(1) <  0; xrange = (index_max(1):-1:index_min(1)) - 1;
+        else;                    xrange = (index_min(1)   :index_max(1)) - 1;
+        end
+        a_set_x = (to_plane(1) + (v_dims(1) * xrange)) ./ v1_to_v2(1);
     
-    ls   = zeros(1, len_a - 1);
-    idxs = zeros(3, len_a - 1);
+        if     v1_to_v2(2) == 0; yrange = [];
+        elseif v1_to_v2(2) <  0; yrange = (index_max(2):-1:index_min(2)) - 1;
+        else;                    yrange = (index_min(2)   :index_max(2)) - 1;
+        end
+        a_set_y = (to_plane(2) + (v_dims(2) * yrange)) ./ v1_to_v2(2);
     
-    dist_to_voxels = (ray_start - init_plane) ./ v_dims;
-    vox_v1_to_v2_2 = v1_to_v2 ./ (2 .* v_dims);
+        if     v1_to_v2(3) == 0; zrange = [];
+        elseif v1_to_v2(3) <  0; zrange = (index_max(3):-1:index_min(3)) - 1;
+        else;                    zrange = (index_min(3)   :index_max(3)) - 1;
+        end
+        a_set_z = (to_plane(3) + (v_dims(3) * zrange)) ./ v1_to_v2(3);
     
-    a_1 = a(2:end);
-    parfor i = 1:len_a-1
-        a_i = a_1(i); a_i_1 = a(i); % Pre-access the values to speed up the code
-        idxs(:, i) = 1 + (dist_to_voxels + ((a_i + a_i_1) .* vox_v1_to_v2_2));
-        ls(i) = d_12 * (a_i - a_i_1);
+        % Get the union of the arrays
+        a = unique([a_min, a_set_x, a_set_y, a_set_z, a_max]);
+        % a = sort([a_min, a_set_x, a_set_y, a_set_z, a_max]);
+    
+        idxs = floor(1 + ((a(2:end) + a(1:end-1)).* v1_to_v2./2 - to_plane) ./ v_dims);
+        ls = norm(v1_to_v2) * diff(a);
     end
-    idxs = min(floor(idxs), index_max);
-    idxs = idxs(:, ls > 1e-14); % Remove any indices with a length of 0 (this can happen due to floating point errors)
-    ls = ls(ls > 1e-14); % Remove any lengths of 0 (this can happen due to floating point errors)
-end
-
-function set_a = get_set_a(start_point, voxels_min, v_dims, i_min, i_max, dist_to_detector)
-    % Get the set of a values for a given coordinate - created for speed reasons
-    if abs(dist_to_detector) < 1e-14 % Avoid floating point errors
-        set_a = []; return % No intersections as the ray is parallel to the plane
-    end
-    len = i_max - i_min + 1;
-    da = v_dims ./ dist_to_detector;
-    set_a = zeros(1, len) + da;
-    set_a(1) = (voxels_min - start_point) ./ dist_to_detector;
-    set_a = cumsum(set_a);
-end
